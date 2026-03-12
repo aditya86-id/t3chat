5183daafa8f4783b1c3197659242678523d26e90
adityakumartiwary <adityatiwary888@gmail.com>
2026-03-12 23:52:31 +0530

    updated_database

---
+++ t3chat/src/app/(auth)/layout.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Authlayout = ({children}) => {
+  return (
+    <>
+    {children}
+    </>
+  )
+}
+
+export default Authlayout
---
+++ t3chat/src/lib/auth-client.js
@@ -0,0 +1,5 @@
+import { createAuthClient } from "better-auth/react"
+export const { signIn,signOut,useSession,signUp} = createAuthClient({
+
+    baseURL:"http://localhost:3000"
+})
